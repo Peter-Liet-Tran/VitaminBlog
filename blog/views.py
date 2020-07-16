c30ad75f69b67f21c437accb8317370e4a741be2
peter <peter@Lenovo>
2020-07-16 14:20:11 -0600

    Change return for view

--- blog/views.py
+++ blog/views.py
@@ -2,5 +2,5 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def home(request):
-    return HttpResponse('<h1>Blog Home</h1>')
+    return HttpResponse('<h1>Blog Home Test</h1>')
 
