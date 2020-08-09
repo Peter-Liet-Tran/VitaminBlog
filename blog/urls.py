2ff3ae6c26bad3b3d338b9fe8169084e5612fd81
peter <peter@Lenovo>
2020-08-09 14:01:50 -0600

    Added pagination so blogs have page filtering and filtering based on user when clicking on username

--- blog/urls.py
+++ blog/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 from . import views
 from .views import (PostListView, PostDetailView, PostCreateView,
-PostUpdateView, PostDeleteView)
+PostUpdateView, PostDeleteView, UserPostListView)
 
 urlpatterns = [
     path('', PostListView.as_view(), name='blog-home'),
+    path('user/<str:username>', UserPostListView.as_view(), name='user-posts'),
     path('post/new/', PostCreateView.as_view(), name='post-create'),
     path('post/<int:pk>/update', PostUpdateView.as_view(), name='post-update'),
     path('post/<int:pk>/delete', PostDeleteView.as_view(), name='post-delete'), 
